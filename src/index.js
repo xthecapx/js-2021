ad59df4c3804a8ffaedb396927e4692e2828a803
xthecapx <xthecapx@gmail.com>
2021-02-24 13:09:11 -0500

    feat: initial setup with babel

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// https://github.com/tc39/proposals/blob/master/finished-proposals.md
+
+// String.prototype.replaceAll (https://github.com/tc39/proposal-string-replaceall)
+
+// Promise.any (https://github.com/tc39/proposal-promise-any)
+
+// WeakRefs (https://github.com/tc39/proposal-weakrefs)
+
+// Logical Assignment Operators (https://github.com/tc39/proposal-logical-assignment)
+
+// Numeric separators (https://github.com/tc39/proposal-numeric-separator)
